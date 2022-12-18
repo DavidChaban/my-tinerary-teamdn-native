2af4016f08b55d30deaffbd355fb71f4bb455ab0
David Chaban <davidchaban01@gmail.com>
2022-12-17 23:26:37 -0300

    Se agrega city Detail Mobile

--- views/CityDetail.js
+++ views/CityDetail.js
@@ -1,14 +1,14 @@
-// import { View, Text } from 'react-native'
-// import React from 'react'
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 
-// export default function CityDetail() {
+export default function CityDetail() {
   
-//   return (
-//     <View>
-//       <Text>City Detail</Text>
-//     </View>
-//   )
-// }
+  return (
+    <View>
+      <Text>City Detail</Text>
+    </View>
+  )
+}
